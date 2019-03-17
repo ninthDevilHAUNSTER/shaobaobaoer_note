707379e1e12b95bc74d7820c9c6c6d5f7e8d2c80
ninthDevilHAUNSTER <luotianhan@126.com>
2019-03-17 10:49:14 +0800

    加了点东西

---
+++ misc/path_changer.py
@@ -0,0 +1,7 @@
+import re
+
+# ![Image text](https://github.com/()/()/raw/master/()/())/x.png)
+
+pattern = "![{image_text}](https://github.com/{github_username}/{github_project_name}/raw/master/{abs_path}/{file_name})"
+
+
